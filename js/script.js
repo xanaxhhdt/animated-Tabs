f65e1131c3847cea8b80ab96111eac1ea9ecc93c
Vladislav <nicochan00@mail.ru>
2022-07-05 20:07:22 +0300

    endind verstka + style

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const tabBtns = Array.from(document.querySelectorAll('#tab_btn'));
+
+tabBtns.forEach(item => {
+   item.addEventListener('click', onTabBtnClick);
+});
+
+function onTabBtnClick(e) {
+   e.preventDefault();
+
+   const btn = e.target.closest('#tab_btn');
+   changeBtn(btn);
+}
+
+function changeBtn(btn) {
+
+}
