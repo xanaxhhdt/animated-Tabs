f3f143838f3d493747a997d3dd8e858d527455c6
Vladislav <nicochan00@mail.ru>
2022-07-05 20:28:40 +0300

    script tabs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 'use strict';
 
 const tabBtns = Array.from(document.querySelectorAll('#tab_btn'));
+const tabIndicator = document.querySelector('#tab_indicator');
+const tabSlide = Array.from(document.querySelectorAll('#tab_slide'));
+
+tabBtns[0].classList.add('active');
+tabSlide[0].classList.add('active');
+
+let activeBtn = tabBtns[0];
+let activeSlide = tabSlide[0];
 
 tabBtns.forEach(item => {
    item.addEventListener('click', onTabBtnClick);
@@ -14,5 +22,25 @@ function onTabBtnClick(e) {
 }
 
 function changeBtn(btn) {
+   if (btn.classList.contains('active')) {
+      return;
+   } 
+   activeBtn.classList.remove('active');
+   btn.classList.add('active');
+   activeBtn = btn;
+   
+   changeIndicator(btn);
+}
 
+function changeIndicator(btn) {
+   const indexBtn = tabBtns.indexOf(btn);
+   tabIndicator.style.left = `calc(${indexBtn} * 100% / 4)`;
+
+   changeSlide(indexBtn);
+}
+
+function changeSlide(index) {
+   activeSlide.classList.remove('active');
+   tabSlide[index].classList.add('active');
+   activeSlide = tabSlide[index];
 }
