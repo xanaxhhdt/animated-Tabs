4a96b280e5a1a1374ea561ac12ba29ef863abc49
Vladislav <nicochan00@mail.ru>
2022-07-05 20:58:46 +0300

    refak. code + endind project

--- js/script.js
+++ js/script.js
@@ -1,46 +1,48 @@
 'use strict';
 
-const tabBtns = Array.from(document.querySelectorAll('#tab_btn'));
-const tabIndicator = document.querySelector('#tab_indicator');
-const tabSlide = Array.from(document.querySelectorAll('#tab_slide'));
+const tabBtns = Array.from(document.querySelectorAll(".tab__btn"));
+const tabIndicator = document.querySelector("#tab_indicator");
+const tabSlide = Array.from(document.querySelectorAll(".tab__slide"));
+const root = document.querySelector(":root");
 
-tabBtns[0].classList.add('active');
-tabSlide[0].classList.add('active');
+const numBtns = tabBtns.length;
+
+root.style.setProperty("--num-btns", numBtns);
+
+tabBtns[0].classList.add("active");
+tabSlide[0].classList.add("active");
 
 let activeBtn = tabBtns[0];
 let activeSlide = tabSlide[0];
 
-tabBtns.forEach(item => {
-   item.addEventListener('click', onTabBtnClick);
+tabBtns.forEach((el) => {
+   el.addEventListener("click", onTabBtnClick);
 });
 
 function onTabBtnClick(e) {
    e.preventDefault();
-
-   const btn = e.target.closest('#tab_btn');
+   const btn = e.target.closest(".tab__btn");
    changeBtn(btn);
 }
 
 function changeBtn(btn) {
-   if (btn.classList.contains('active')) {
+   if (btn.classList.contains("active")) {
       return;
-   } 
-   activeBtn.classList.remove('active');
-   btn.classList.add('active');
+   }
+   activeBtn.classList.remove("active");
+   btn.classList.add("active");
    activeBtn = btn;
-   
    changeIndicator(btn);
 }
 
 function changeIndicator(btn) {
    const indexBtn = tabBtns.indexOf(btn);
-   tabIndicator.style.left = `calc(${indexBtn} * 100% / 4)`;
-
+   tabIndicator.style.left = `calc(${indexBtn}*100%/${numBtns})`;
    changeSlide(indexBtn);
 }
 
 function changeSlide(index) {
-   activeSlide.classList.remove('active');
-   tabSlide[index].classList.add('active');
+   activeSlide.classList.remove("active");
+   tabSlide[index].classList.add("active");
    activeSlide = tabSlide[index];
 }
